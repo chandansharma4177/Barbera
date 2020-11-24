6db24898591bf8b888fc64a69e326d2322659257
chandansharma4177 <71401959+chandansharma4177@users.noreply.github.com>
2020-11-24 20:56:59 +0530

    added submit page with user details

--- submit.js
+++ submit.js
@@ -31,28 +31,109 @@ function dsisplayFinalCart(){
 dsisplayFinalCart()
 
 
-// var date = new Date();
-// date.setDate(date.getDate()-1);
-//
-// console.log(date);
-// // $(".datepicker").value = date;
-// $('.datepicker').datepicker({
-//   format: "mm/dd/yyyy",
-//   startDate:date
-// });
 
 var date = new Date();
-
+var d;
 var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
 $('.datepicker').datepicker({
+  onSelect: function (selectedDate) {
+    d = selectedDate;   },
   dateFormat: "dd/mm/yy",
 
     minDate: today
 });
 
 
-$(".dateDisplay")[0].innerText = $(".datepicker")[0].value
 
 
-$('.timepicker').wickedpicker();
+
+
+$('.submitButton')[0].addEventListener('click', function(){
+  var cartItemNumbers = localStorage.getItem("cartItemNumbers");
+  cartItemNumbers = JSON.parse(cartItemNumbers)
+  var cartItems = localStorage.getItem("itemList");
+  cartItems = JSON.parse(cartItems);
+
+
+
+  var name = $('.name')[0].value;
+  var mobileNumber = $('.mobile')[0].value;
+  var address = $('.address')[0].value;
+  var date = d;
+  var time = $('.timepicker')[0].value;
+
+  if(cartItemNumbers === 0){
+    alert("Please select some items")
+  }
+  else{
+    if(name.length ===0){
+      alert("Please enter the name")
+    }
+    else{
+      if(mobileNumber.length !== 10 ){
+        alert("Please enter valid mobile number")
+      }
+      else{
+        if(address.length === 0){
+          alert("Please enter valid address")
+        }
+        else{
+          if(date === undefined){
+            alert("Please enter valid date")
+          }
+          else{
+            if(time === ""){
+              alert("Please enter valid time")
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+  if(name.length !==0 && mobileNumber.length === 10  && address.length !==0 && time !==""){
+
+    var totalPayableMoney = 0;
+    Object.values(cartItems).map(item => {
+      var quantityNumber = parseFloat(item.quantity_value);
+      var priceNumber = parseFloat(item.price_value.replace("Rs ", ""));
+
+      var totalPriceOfItem =priceNumber*quantityNumber ;
+      totalPayableMoney += totalPriceOfItem;
+
+      console.log(item.title_name);
+      console.log(item.price_value);
+      console.log(item.quantity_value);
+      console.log(totalPriceOfItem);
+
+    })
+
+    
+    console.log(name);
+    console.log(mobileNumber);
+    console.log(address);
+    console.log(date);
+    console.log(time);
+
+    if(cartItems != null){
+      cartItems = {}
+
+      localStorage.setItem('itemList', JSON.stringify(cartItems));
+    }
+
+
+
+    cartItemNumbers = 0;
+
+    localStorage.setItem('cartItemNumbers', JSON.stringify(cartItemNumbers));
+
+  }
+
+
+})
